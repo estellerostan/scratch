d4a638ff37ae41652ecd88e4f24a576a6a397fdb
Estelle Rostan <estellerostan@pm.me>
2020-08-15 21:05:45 +0200

    Add code and tests

--- test/scratch/core_test.clj
+++ test/scratch/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [scratch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest pow-test
+  (testing "unity"
+    (is (= 1 (pow 1 1))))
+
+  (testing "square integers"
+    (is (= 9 (pow 3 2))))
+
+  (testing "0^0"
+    (is (= 1 (pow 0 0)))))
