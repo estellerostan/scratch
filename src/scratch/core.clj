527bc8dfe5b0ae742d9a9d2beb490598ec09b116
Estelle Rostan <estellerostan@pm.me>
2020-08-16 00:46:28 +0200

    Use Cheshire to parse ucr data

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -4,3 +4,6 @@
   "Raises base to the given power. For instance, (pow 3 2) returns three squared, or nine."
   [base power]
   (apply * (repeat power base)))
+
+(use 'cheshire.core)
+(parse-string (slurp "resources/ucr-normalized-2008.json"))
