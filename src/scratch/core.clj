1695f1824aebc70b1e31ef4eff59ddb2456e92ec
Estelle Rostan <estellerostan@pm.me>
2020-08-18 01:11:43 +0200

    Print the 10 worst counties for DUIs

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,6 +1,7 @@
 (ns scratch.core
   (:require
-   [cheshire.core :as cheshire]))
+   [cheshire.core :as cheshire]
+   [clojure.pprint :as p]))
 
 (defn pow
   "Raises base to the given power. For instance, (pow 3 2) returns three squared, or nine."
@@ -9,4 +10,4 @@
 
 (def data (cheshire/parse-string (slurp "resources/ucr-normalized-2008.json") true))
 
-(->> data (map :driving_under_influence) frequencies)
+(->> data (map :driving_under_influence) frequencies (sort-by key) (take-last 10) p/pprint)
