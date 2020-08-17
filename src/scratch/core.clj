a11c338dadbb0ef040bede75336218a52497c5dd
Estelle Rostan <estellerostan@pm.me>
2020-08-18 01:10:20 +0200

    Replace use by :require for the Cheshire namespace

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,11 +1,12 @@
-(ns scratch.core)
+(ns scratch.core
+  (:require
+   [cheshire.core :as cheshire]))
 
 (defn pow
   "Raises base to the given power. For instance, (pow 3 2) returns three squared, or nine."
   [base power]
   (apply * (repeat power base)))
 
-(use 'cheshire.core)
-(def data (parse-string (slurp "resources/ucr-normalized-2008.json") true))
+(def data (cheshire/parse-string (slurp "resources/ucr-normalized-2008.json") true))
 
 (->> data (map :driving_under_influence) frequencies)
