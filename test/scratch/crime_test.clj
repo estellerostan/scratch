b922077eb7091db2fc7db6cbe8ae2d3b932878d9
Estelle Rostan <estellerostan@pm.me>
2020-08-31 02:15:59 +0200

    Add crime prevalence test

--- test/scratch/crime_test.clj
+++ test/scratch/crime_test.clj
@@ -4,3 +4,7 @@
 
 (deftest fips-code-test
   (is (= "12345" (fips-code {:fips_state_code "12" :fips_county_code "345"}))))
+
+(deftest most-prevalent-test
+  (testing "prevalence calculation"
+   (is (= (float 21/27158) (get-in (most-prevalent "resources/ucr_normalized_2008.json" :arson) [0 1])))))
