3ebd66d2d59ac3ae28b0c89d91997a3981ec3d96
Estelle Rostan <estellerostan@pm.me>
2020-09-01 23:02:25 +0200

    Add gravity force calculation

--- src/scratch/rocket.clj
+++ src/scratch/rocket.clj
@@ -42,3 +42,43 @@
   "Prepares a craft for launch from an equatorial space center."
   [craft]
   (merge craft initial-space-center))
+
+(defn magnitude
+  "What's the radius of a given set of cartesian coordinates?"
+  [c]
+  ; By the Pythagorean theorem...
+  (Math/sqrt (+ (Math/pow (:x c) 2)
+                (Math/pow (:y c) 2)
+                (Math/pow (:z c) 2))))
+
+(defn cartesian->spherical
+  "Converts a map of Cartesian coordinates :x, :y, and :z to spherical coordinates :r, :theta, and :phi."
+  [c]
+  (let [r (magnitude c)]
+    {:r r
+     :theta (Math/acos (/ (:z c) r))
+     :phi   (Math/atan (/ (:y c) (:x c)))}))
+
+(defn spherical->cartesian
+  "Converts spherical to Cartesian coordinates."
+  [c]
+  {:x (* (:r c) (Math/sin (:theta c)) (Math/cos (:phi c)))
+   :y (* (:r c) (Math/sin (:theta c)) (Math/sin (:phi c)))
+   :z (* (:r c) (Math/cos (:phi c)))})
+
+(def g "Acceleration of gravity in meters/s^2" -9.8)
+
+(defn gravity-force
+  "The force vector, each component in Newtons, due to gravity."
+  [craft]
+  ; Since force is mass times acceleration...
+  (let [total-force (* g (mass craft))]
+    (-> craft
+        ; Now we'll take the craft's position
+        :position
+        ; in spherical coordinates,
+        cartesian->spherical
+        ; replace the radius with the gravitational force...
+        (assoc :r total-force)
+        ; and transform back to Cartesian-land
+        spherical->cartesian)))
